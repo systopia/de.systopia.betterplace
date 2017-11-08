399d834c7d845298bde83c86a92491ccc7c4d18b
systopia <endres@systopia.de>
2017-11-08 10:29:40 +0100

    boilerplate code

---
+++ CRM/Betterplace/Profile.php
@@ -0,0 +1,151 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Betterplace Integration                       |
+| Copyright (C) 2017 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+|         J. Schuppe (schuppe@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Betterplace_ExtensionUtil as E;
+
+/**
+ * Profiles define how incoming submissions from
+ *  a donation page are processed in CiviCRM
+ */
+class CRM_Betterplace_Profile {
+
+  /** caches the profile objects */
+  protected static $_profiles = NULL;
+
+  protected $name = NULL;
+  protected $data = NULL;
+
+  public function __construct($name, $data) {
+    $this->name = $name;
+    $this->data = $data;
+  }
+
+
+  /**
+   * TODO
+   */
+  public function matches($form_id) {
+    $selector = $this->getAttribute('selector');
+    $form_ids = explode(',', $selector);
+    return in_array($form_id, $form_ids);
+  }
+
+  /**
+   * TODO
+   */
+  public function getAttribute($attribute_name) {
+    if (isset($this->data[$attribute_name])) {
+      return $this->data[$attribute_name];
+    } else {
+      return NULL;
+    }
+  }
+
+  /**
+   * TODO
+   */
+  public function setAttribute($attribute_name, $value) {
+    // TODO: check if attribute wanted, value acceptable
+    $this->data[$attribute_name] = $value;
+  }
+
+
+  /**
+   * Verify if given profile is valid, (also wrt the other profiles)
+   * @throws Exception
+   */
+  public function verifyProfile() {
+    // TODO: check
+    //  data of this profile consistent?
+    //  conflicts with other profiles?
+  }
+
+  /**
+   * Verify if given profile is valid, (also wrt the other profiles)
+   */
+  public function saveProfile() {
+    $this->verifyProfile();
+    self::storeProfiles();
+  }
+
+
+
+
+  /**
+   * this is the "factory default" profile
+   */
+  public static function createDefaultProfile() {
+    return new CRM_Betterplace_Profile('default', array(
+      'selector'          => '',
+      'financial_type_id' => 1, // "Donation"
+      'campaign_id'       => '',
+      'pi_creditcard'     => 1, // "Credit Card"
+      'pi_sepa'           => 5, // "EFT"
+      'pi_paypal'         => 3, // "Debit"
+      'groups'            => '',
+    ));
+  }
+
+  /**
+   * retrieves the rigth profile for the given
+   *  form ID. Returns the default profile if none
+   *  found.
+   */
+  public static function getProfileForForm($form_id) {
+    $profiles = self::getProfiles();
+    foreach ($profiles as $profile) {
+      if ($profile->matches($form_id)) {
+        return $profile;
+      }
+    }
+
+    // no profile found?
+    return $profiles['default'];
+  }
+
+  /**
+   * Get the (raw) list of all profiles
+   */
+  public static function getProfiles() {
+    if (self::$_profiles === NULL) {
+      self::$_profiles = array();
+      $profiles_data = CRM_Core_BAO_Setting::getItem('de.systopia.betterplace', 'betterplace_profiles');
+      foreach ($profiles_data as $profile_name => $profile_data) {
+        self::$_profiles[$profile_name] = new CRM_Betterplace_Profile($profile_name, $profile_data);
+      }
+    }
+
+    // make sure the default profile is there
+    if (!isset(self::$_profiles['default'])) {
+      self::$_profiles['default'] = self::createDefaultProfile();
+      self::storeProfiles();
+    }
+
+    return self::$_profiles;
+  }
+
+
+  /**
+   * Set the (raw) list of all profiles
+   */
+  public static function storeProfiles() {
+    $profile_data = array();
+    foreach (self::$_profiles as $profile_name => $profile) {
+      $profile_data[$profile_name] = $profile->profile;
+    }
+    CRM_Core_BAO_Setting::setItem($profile_data, 'de.systopia.betterplace', 'betterplace_profiles');
+  }
+}
---
+++ betterplace.php
@@ -0,0 +1,166 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Betterplace Integration                       |
+| Copyright (C) 2017 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+|         J. Schuppe (schuppe@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+require_once 'betterplace.civix.php';
+use CRM_Betterplace_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function betterplace_civicrm_config(&$config) {
+  _betterplace_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function betterplace_civicrm_xmlMenu(&$files) {
+  _betterplace_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function betterplace_civicrm_install() {
+  _betterplace_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function betterplace_civicrm_postInstall() {
+  _betterplace_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function betterplace_civicrm_uninstall() {
+  _betterplace_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function betterplace_civicrm_enable() {
+  _betterplace_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function betterplace_civicrm_disable() {
+  _betterplace_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function betterplace_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _betterplace_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function betterplace_civicrm_managed(&$entities) {
+  _betterplace_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function betterplace_civicrm_caseTypes(&$caseTypes) {
+  _betterplace_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function betterplace_civicrm_angularModules(&$angularModules) {
+  _betterplace_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function betterplace_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _betterplace_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function betterplace_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function betterplace_civicrm_navigationMenu(&$menu) {
+  _betterplace_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => E::ts('The Page'),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _betterplace_civix_navigationMenu($menu);
+} // */
