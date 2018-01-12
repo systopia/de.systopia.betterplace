a317d66d1742bdf7c44805d2d490cbd01bf11348
Tobias Jordans <t@tobiasjordans.de>
2018-01-12 09:51:41 +0100

    Update wording on settings
    We need to separate betterplace.org with our offer that we call "direkt".

--- CRM/Betterplace/Page/Settings.php
+++ CRM/Betterplace/Page/Settings.php
@@ -1,18 +1,18 @@
 <?php
-/*-------------------------------------------------------+
-| SYSTOPIA Betterplace Integration                       |
-| Copyright (C) 2017 SYSTOPIA                            |
-| Author: B. Endres (endres@systopia.de)                 |
-|         J. Schuppe (schuppe@systopia.de)               |
-+--------------------------------------------------------+
-| This program is released as free software under the    |
-| Affero GPL license. You can redistribute it and/or     |
-| modify it under the terms of this license which you    |
-| can read by viewing the included agpl.txt or online    |
-| at www.gnu.org/licenses/agpl.html. Removal of this     |
-| copyright header is strictly prohibited without        |
-| written permission from the original author(s).        |
-+--------------------------------------------------------*/
+/*------------------------------------------------------------+
+| SYSTOPIA betterplace.org Spendenformular Direkt Integration |
+| Copyright (C) 2017 SYSTOPIA                                 |
+| Author: B. Endres (endres@systopia.de)                      |
+|         J. Schuppe (schuppe@systopia.de)                    |
++-------------------------------------------------------------+
+| This program is released as free software under the         |
+| Affero GPL license. You can redistribute it and/or          |
+| modify it under the terms of this license which you         |
+| can read by viewing the included agpl.txt or online         |
+| at www.gnu.org/licenses/agpl.html. Removal of this          |
+| copyright header is strictly prohibited without             |
+| written permission from the original author(s).             |
++-------------------------------------------------------------*/
 
 use CRM_Betterplace_ExtensionUtil as E;
 
@@ -20,7 +20,7 @@ class CRM_Betterplace_Page_Settings extends CRM_Core_Page {
 
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(E::ts('Betterplace Settings'));
+    CRM_Utils_System::setTitle(E::ts('betterplace.org Direkt Settings'));
 
     $profiles = array();
     foreach (CRM_Betterplace_Profile::getProfiles() as $profile_name => $profile) {
